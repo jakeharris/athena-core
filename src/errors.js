3f10e4dde67651580a420eccd3504d9eca21fade
Jake Harris <milieu@javakat.io>
2016-12-12 14:14:02 -0600

    Add NoContentError ?, comments to describe errors

--- src/errors.js
+++ src/errors.js
@@ -1,7 +1,17 @@
+// Indicates we received an invalid number of parameters (too few).
 class ParameterCountError extends Error {
     constructor(message) {
         super(message)
+        this.name = 'ParameterCountError'
+    }
+}
+// Indicates we should be returning content, but all we have is something blank.
+class NoContentError extends Error {
+    constructor(message) {
+        super(message)
+        this.name = 'NoContentError'
     }
 }
 
-module.exports = ParameterCountError
+module.exports.ParameterCountError = ParameterCountError
+module.exports.NoContentError = NoContentError
