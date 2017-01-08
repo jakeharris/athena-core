56eec19add40518e0abca79cb0e9831666e02929
Jake Harris <milieu@javakat.io>
2017-01-08 14:28:10 -0600

    Remove timestamps on this one too

--- models/view.js
+++ models/view.js
@@ -3,6 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var View = sequelize.define('View', {
     userID: DataTypes.BIGINT.UNSIGNED
   }, {
+    timestamps: false,
     classMethods: {
       associate: function(models) {
         // associations can be defined here
