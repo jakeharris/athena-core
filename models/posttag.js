f45a71f8132d1f512c1f9cdf4755226760536841
Jake Harris <milieu@javakat.io>
2017-01-08 14:26:08 -0600

    Formally create join table model in sequelize so it can find the db table

--- models/posttag.js
+++ models/posttag.js
@@ -1,16 +1,16 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Post = sequelize.define('Post', {
-    url: DataTypes.TEXT
+  var PostTag = sequelize.define('PostTag', {
+    
   }, {
     timestamps: false,
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Post.hasMany(models.View)
-        Post.belongsToMany(models.Tag, {through: models.PostTag, timestamps: false})
+        PostTag.belongsTo(models.Post)
+        PostTag.belongsTo(models.Tag)
       }
     }
   });
-  return Post;
+  return PostTag;
 };
--- models/tag.js
+++ models/tag.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Tag.belongsToMany(models.Post, {through: 'PostTag', timestamps: false})
+        Tag.belongsToMany(models.Post, {through: models.PostTag, timestamps: false})
       }
     }
   });
