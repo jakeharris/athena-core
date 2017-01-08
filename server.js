ea0db4a97ec7f370b80b183187f19fe86e3bcbf0
Jake Harris <milieu@javakat.io>
2017-01-08 14:30:38 -0600

    Add express server to let discord client query

---
+++ server.js
@@ -0,0 +1,29 @@
+const Express = require('express'),
+      BodyParser = require('body-parser'),
+      Core = require('./src/core').Core,
+      Models = require('./src/core').Models
+
+let server = Express()
+
+server.use(BodyParser.json())
+
+server.post('/post', (request, response) => {
+
+    console.log(request.body)
+
+    Core.onRequest(request.body).then((url) => {
+        // url will be a string that is EITHER
+        // a post URL like we expect, or an
+        // error message string if things went
+        // sour
+        response.send(url)
+    }).then(() => {
+        Core.fetchFromTumblr()
+    })
+})
+
+Models.sequelize.sync().then(() => {
+    console.log('listening on port 3000...')
+    server.listen(3000)
+})
+
