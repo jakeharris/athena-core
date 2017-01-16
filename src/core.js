081f2256135c6e8e1b68d934f2b911ccb5db4a88
Jake Harris <milieu@javakat.io>
2017-01-16 14:58:32 -0600

    Fix (???) final SequelizeUniqueConstraintError, remove extra logging

--- src/core.js
+++ src/core.js
@@ -69,16 +69,15 @@ class Core {
                             + 'AND Tags.name in ("' + tags.join('","') + '") '
                         + 'GROUP BY Posts.id '
                         + 'HAVING COUNT(Tags.name) >= ' + tags.length
-                    + ') as Post, Views '
+                    + ') as Post, Views'
                     + 'WHERE (' 
                         + '(Views.PostId = Post.id AND Views.UserId != ' + id + ') '
                         + 'OR NOT EXISTS (SELECT * FROM Posts,Views WHERE Post.id = Views.PostId)'
-                    + ')',
+                    + ')'
+                    + 'GROUP BY Post.id, Post.url',
                 { model: Models.Post }
             )
                 .then((posts) => { 
-                    console.log('%%%%% PRIMARY QUERY RESULTS %%%%%')
-                    console.log(posts)
                     if(typeof (posts[0].dataValues) === 'undefined' )
                         reject(posts[0])
                     resolve(posts[0].dataValues)
